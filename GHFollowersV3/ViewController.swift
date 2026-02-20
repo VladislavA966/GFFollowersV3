912a7caa856600ab8ca7bcae977c49bcd20c5fbb
v_afonin <Vladislav.Afonin@mbank.kg>
2026-02-20 15:07:45 +0600

    Initial Commit

---
+++ GHFollowersV3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowersV3
+//
+//  Created by Владилсав on 20/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GHFollowersV3Tests/GHFollowersV3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersV3Tests.swift
+//  GHFollowersV3Tests
+//
+//  Created by Владилсав on 20/2/26.
+//
+
+import Testing
+@testable import GHFollowersV3
+
+struct GHFollowersV3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
